dfe654e93a79121e372153e47be7948a6a9e808c
jazzrelax <gmanzanares.arq@gmail.com>
2023-09-19 16:31:03 -0600

    Se agregó la carpeta m2s6

---
+++ m2s6/m2s6/src/main/java/org/bedu/practicas/m2s6/M2s6Application.java
@@ -0,0 +1,13 @@
+package org.bedu.practicas.m2s6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2s6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2s6Application.class, args);
+	}
+
+}
---
+++ m2s6/m2s6/src/test/java/org/bedu/practicas/m2s6/M2s6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.practicas.m2s6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2s6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
